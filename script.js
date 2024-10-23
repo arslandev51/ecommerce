2d9b45834ac62eb3a12b72a7803f7631339762ce
arslandev51 <161108212+arslandev51@users.noreply.github.com>
2024-10-23 08:05:12 +0500

    Update script.js
    changes in script file

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
 
 if(bar){
   bar.addEventListener('click', ()=>{
-    nav.classList.add('active');
+    nav.classList.toggle('active');
   });
 };
-if(bar){
-  bar.addEventListener
+
+if(close){
+  close.addEventListener('click',()=>{
+    nav.classList.remove('active');
+  });
 }
-
-
